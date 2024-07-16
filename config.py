3e3361ea6ba04500e1c2b81212aaddae0b0815d9
Alexon Uthayakumar <authayakumar@reseau.free.fr>
2024-07-16 15:57:05 +0200

    (wip) start with config files

--- config.py
+++ config.py
@@ -1,12 +1,29 @@
+import os
 import environ
 
 env = environ.Env()
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = env("DATABASE_URI")
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+
+    # Database configuration
+    DB_ENGINE = env("DB_ENGINE")
+    DB_USERNAME = env("DB_USERNAME")
+    DB_PASSWORD = env("DB_PASSWORD")
+    DB_HOST = env("DB_HOST")
+    DB_PORT = env("DB_PORT")
+    DB_NAME = env("DB_NAME")
+
+    if DB_ENGINE == "sqlite":
+        SQLALCHEMY_DATABASE_URI = f"{DB_ENGINE}:///{base_dir}/{DB_NAME}"
+    else:
+        SQLALCHEMY_DATABASE_URI = (
+            f"{DB_ENGINE}://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        )
+
     DEBUG = env.bool("DEBUG", default=False)
-    apps = ["authentication"]
+    installed_apps = ["authentication"]
 
 
 class DevelopmentConfig(Config):
